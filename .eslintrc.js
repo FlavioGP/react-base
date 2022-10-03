8b2b432041a0c9150f8709e566472be6942fcf84
= <flavio.gpinheiro@hotmail.com>
2022-10-03 16:40:04 +0000

    configurando axios

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/react-in-jsx-scope': 0,
+    'no-console': 0,
   },
 };
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Container } from '../../styles/GlobalStyles'
 import { Title, Paragraph } from './styled'
 
+
 export default function Login(){
+
   return(
     <Container>
       <Title>
